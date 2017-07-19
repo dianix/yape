66007e8b7f1530330da6149e538fd948408e2293
dianix <dianavazquez.dcg@gmail.com>
2017-07-19 14:36:18 -0500

    funcion para validacion hecha

--- public/assets/js/registro.js
+++ public/assets/js/registro.js
@@ -1,7 +1,19 @@
 var cargarPagina = function () {
-    console.log("hola mundo")
+    //llamo elementos del dom para obtener datos
+    var areaTel = $('#areaTelefono'); 
+    //console.log(areaTel);   
+    areaTel.keyup(validarTel); 
+};
 
-}
+var validarTel = function () {
+    var $botonEnviar = $("#botonEnviar");
+    var termCond = $('#termCond');
+    if (termCond.prop("checked") === true && $(this).val().trim().length === 10 ) {
+        $botonEnviar.removeAttr("disabled");
+    } else {
+        $botonEnviar.attr("disabled", true);
+    }
+};
 
 
 
