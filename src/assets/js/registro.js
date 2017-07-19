417df323c326f67e9f4d625112762c421699daa1
dianix <dianavazquez.dcg@gmail.com>
2017-07-19 17:12:55 -0500

    commit

--- src/assets/js/registro.js
+++ src/assets/js/registro.js
@@ -1,7 +1,7 @@
 var cargarPagina = function () {
     //llamo elementos del dom para obtener datos
     var botonEnviar = $("#botonEnviar");
-    var areaTel = $('#areaTelefono');   
+    var areaTel = $('#areaTelefono');
     areaTel.keyup(validarTel);
     botonEnviar.submit(generarCodigo);
 };
@@ -10,16 +10,17 @@ var validarTel = function () {
     var botonEnviar = $("#botonEnviar");
     var termCond = $('#termCond');
     //condiciones para que se habilite el boton
-    if (termCond.prop("checked")==true && $(this).val().trim().length == 10 ) {
-        botonEnviar.removeAttr("disabled");        
-    } else {
+    if (termCond.prop("checked") == true && $(this).val().trim().length == 10) {
+        botonEnviar.removeAttr("disabled");
+    } else if (termCond.prop("checked") == false && $(this).val().trim().length != 10) {
         botonEnviar.attr("disabled", true);
     }
 };
 
-var generarCodigo = function(e){
+var generarCodigo = function (e) {
     e.preventDefault();
-    location.href = "views/codigo.html";
+    alert("generar codigo")
+    //location.href = "views/codigo.html";
 }
 
 
