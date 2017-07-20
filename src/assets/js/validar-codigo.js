5bb439cc480a479caa85d04f245b6df1b94190e9
dianix <dianavazquez.dcg@gmail.com>
2017-07-20 15:22:55 -0500

    iniciando funcion para validar codigo

--- src/assets/js/validar-codigo.js
+++ src/assets/js/validar-codigo.js
@@ -1,10 +1,20 @@
+// elementos de html
 var areaCodigo = $("#areaCodigo");
 var numTel = $("#numTel");
+//valores guardados de la pagina anterior para usar en esta
+var codigoOriginal = localStorage.getItem('codigoUsuario');
+var telUsuario = localStorage.getItem('telUsuario');
+
 
 var cargarPagina = function () { 
-    console.log(localStorage.getItem('codigoUsuario'),localStorage.getItem('telUsuario'))
+    //mostrar telefono registrado en el html
     numTel.text(localStorage.getItem('telUsuario'))
+    //enviar código generado al usuario
+    alert("Tu código de validación es: " + localStorage.getItem('codigoUsuario'));
+    areaCodigo.keyup(validarCodigo)
 };
+
+
 
 
 
