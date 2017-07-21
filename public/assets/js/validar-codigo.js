f5ffa4955845d96dae478d6b088e568beb4b198f
dianix <dianavazquez.dcg@gmail.com>
2017-07-21 10:05:39 -0500

    atrapada en validacion de código aun

--- public/assets/js/validar-codigo.js
+++ public/assets/js/validar-codigo.js
@@ -4,39 +4,45 @@ var url = "http://localhost:3000/api/resendCode";
 var areaCodigo = $("#areaCodigo");
 var numTel = $("#numTel");
 //valores guardados de la pagina anterior para usar en esta
-var codigoInicial = localStorage.getItem('codigoUsuario');
+var codigoActual = localStorage.getItem('codigoUsuario');
 var telUsuario = localStorage.getItem('telUsuario');
 
 var codigoNuevo = 0;
 
-var cargarPagina = function () { 
+var cargarPagina = function () {
     //mostrar telefono registrado en el html
     numTel.text(telUsuario)
     //enviar código generado al usuario
-    alert("Tu código de validación es: " + codigoInicial);
+    alert("Tu código de validación es: " + codigoActual);
     areaCodigo.keyup(recibirCodigo)
 };
 
-var recibirCodigo = function(){
-    console.log($(areaCodigo).val().length)
-    if($(areaCodigo).val().length == 6){
-        var codigoUsuario = $(areaCodigo).val();
-        validarCodigo(codigoUsuario,codigoInicial);
+var recibirCodigo = function () {
+    if (areaCodigo.val().length == codigoActual.length) {
+        var codigoUsuario = areaCodigo.val()
+        validarCodigo(codigoUsuario, codigoActual);
     }
 };
 
-var validarCodigo = function(codigoUsuario,codigoGenerado){
-    console.log(codigoUsuario,codigoGenerado)
-	if(codigoUsuario == codigoGenerado){
-		location.href = "regUser.html";
-	} else {
-		newCode();
-	}
+var validarCodigo = function (codigoUsuario, codigoGenerado) {
+    if (codigoUsuario == codigoGenerado) {
+        console.log("el cod es igual")
+        //location.href = "datos-usuario.html";
+    } else if (codigoUsuario != codigoGenerado) {
+        console.log("el cod NO es igual")
+        generarCodigoNuevo();
+    }
 };
 
-var generarCodigoNuevo = function(){
-    $.post(url, {'phone':telUsuario,'terms':true}, function(resp){
-        alert(resp.message + ": " + resp.data)
+var generarCodigoNuevo = function () {
+    $.post(url, {
+        'phone': telUsuario,
+        'terms': true
+    }, function (req) {
+        codigoNuevo = req.data
+        alert(req.message + ": " + codigoNuevo)
+        codigoActual = codigoNuevo;
+        console.log(codigoActual)
     })
 }
 
